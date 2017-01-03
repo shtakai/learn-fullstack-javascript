b9398fc5b6b6b849327617e872eb3cc3dc6cf829
Alyson T <shtakai@gmail.com>
2017-01-03 15:58:14 +0900

    use api router

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import config from './config'
+import apiRouter from './api'
 
 import express from 'express'
 const server = express()
@@ -8,6 +9,8 @@ server.use(express.static('public'))
 server.get('/', (req, res) => {
   res.send('Hello express')
 })
+
+server.use('/api', apiRouter)
 
 server.listen(config.port, () => {
   console.info('express listening on port ', config.port)
