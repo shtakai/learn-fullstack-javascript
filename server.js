149817b6acb3864234b8f38787526090210b03fc
Alyson T <shtakai@gmail.com>
2017-01-03 15:17:01 +0900

    attach to server

---
+++ server.js
@@ -0,0 +1,9 @@
+import https from 'https';
+
+https.get('https://www.lynda.com', res => {
+  console.log(`Response status code: ${res.statusCode}`);
+
+  res.on('data', chunk => {
+    console.log(chunk.toString());
+  })
+})
