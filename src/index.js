03dc2366e3943ac0a14c5a1ab37c8087a8e3f927
Alyson T <shtakai@gmail.com>
2017-01-03 18:46:07 +0900

    Header component

--- src/index.js
+++ src/index.js
@@ -1,20 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const App = (props) => {
+const Header = ({ message }) => {
   return(
-    <h2 className="text-center">
-      {props.headerMessage}
-    </h2>
+      <h2 className="Header text-center">
+        {message}
+      </h2>
   )
 }
 
-App.propTypes = {
-  headerMessage: React.PropTypes.string
+Header.propTypes = {
+  message: React.PropTypes.string
 }
 
-App.defaultProps = {
-  headerMessage: 'hellooo'
+const App = () => {
+  return(
+    <div className="App">
+      <Header message="Naming Contests" />
+      <div>
+        {Math.random()}
+      </div>
+    </div>
+  )
 }
 
 ReactDOM.render(
