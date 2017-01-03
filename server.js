4b1634632f29d4cd1451096980957d4fac5ff03d
Alyson T <shtakai@gmail.com>
2017-01-03 18:02:56 +0900

    start react

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ import express from 'express'
 const server = express()
 
 server.set('view engine', 'ejs')
+server.use(express.static('public'))
 
 server.get('/', (req, res) => {
   res.render('index', {
