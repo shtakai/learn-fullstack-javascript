f69b5cefb94a278df43de575a7f83f46b92d1c85
Alyson T <shtakai@gmail.com>
2017-01-03 15:25:07 +0900

    removed stream

--- server.js
+++ server.js
@@ -4,10 +4,3 @@ const server = http.createServer();
 
 server.listen(8080);
 
-server.on('request', (req, res) => {
-  res.write('Hello http\n');
-  setTimeout(() => {
-    res.write('I can stream\n');
-    res.end();
-  }, 3000);
-});
