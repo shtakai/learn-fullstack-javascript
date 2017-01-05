bdfeb4f182a3e7b3946b864c061fa9867e0e2b5b
Alyson T <shtakai@gmail.com>
2017-01-05 12:24:25 +0900

    component statte

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,24 @@
 import React from 'react'
 import Header from './Header'
 
-const App = () => {
-  return(
-    <div className="App">
-      <Header message="Naming Contests" />
-      <div>
-        {Math.random()}
+class App extends React.Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      pageHeader: 'Naming Contests'
+    }
+  }
+
+  render (){
+    return(
+      <div className="App">
+        <Header message={this.state.pageHeader}/>
+        <div>
+          ...
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default App
